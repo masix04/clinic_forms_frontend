341e213f8b75f91780bd5f405686e6e87f97889a
muhammad.ahmed <muhammad.ahmed@sixlogics.com>
2021-11-12 10:36:56 +0500

    copied the file of universal-branch

---
+++ src/app/html-form-not-in-use/html-form-not-in-use.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'html-form-not-in-use',
+    template:'./html-form-not-in-use.component.html',
+    styleUrls:['./html-form-not-in-use.component.css'],
+})
+class HtmlNotInUse{
+    
+}
